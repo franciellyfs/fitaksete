4ade0edb761a146c0afc9622d93f329601099c55
Desenvolvimento 05 <Desenvolvimento 05@DEV5-PC>
2016-04-11 15:54:00 -0300

    Importação de font lato google, css.

--- index.php
+++ index.php
@@ -11,6 +11,8 @@
     <link href="css/bootstrap.min.css" rel="stylesheet">
     <link href="css/principal.css" rel="stylesheet">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
+    <!-- Fonte -->
+    <link href='http://fonts.googleapis.com/css?family=Lato&subset=latin,latin-ext' rel='stylesheet' type='text/css'>
   </head>
   <body>
     <!-- Cabecalho -->
